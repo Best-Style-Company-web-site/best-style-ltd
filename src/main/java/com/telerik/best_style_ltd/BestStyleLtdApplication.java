1d3c329eddaf97bb1b71ba9b96e62aa204b0ceb6
Rado <markov.r@abv.bg>
2018-07-25 17:56:49 +0300

    Initial project create.

---
+++ src/main/java/com/telerik/best_style_ltd/BestStyleLtdApplication.java
@@ -0,0 +1,12 @@
+package com.telerik.best_style_ltd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestStyleLtdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestStyleLtdApplication.class, args);
+    }
+}
