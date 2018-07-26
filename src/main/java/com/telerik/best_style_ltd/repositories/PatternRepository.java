88af856e63a0decf272eb12b9957c878590a2454
oxanahr <oxana.hristova@gmail.com>
2018-07-26 12:31:06 +0300

    PatternRepository interface implementation

--- src/main/java/com/telerik/best_style_ltd/repositories/PatternRepository.java
+++ src/main/java/com/telerik/best_style_ltd/repositories/PatternRepository.java
@@ -1,6 +1,15 @@
 package com.telerik.best_style_ltd.repositories;
 
+import com.telerik.best_style_ltd.entities.Pattern;
+
+import java.util.List;
+
 public interface PatternRepository {
 
+     void addPattern(Pattern p);
+     void updatePattern(Pattern p);
+     List<Pattern> listPattern();
+     Pattern getPatternById(int id);
+     void removePattern(int id);
 
 }
