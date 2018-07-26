5ba6e3970863b51a9eb40e743006624c2f65c02c
Rado <markov.r@abv.bg>
2018-07-26 18:09:28 +0300

    bla bla

--- src/main/java/com/telerik/best_style_ltd/repositories/UserRepositoryImpl.java
+++ src/main/java/com/telerik/best_style_ltd/repositories/UserRepositoryImpl.java
@@ -1,12 +1,32 @@
 package com.telerik.best_style_ltd.repositories;
 
 import com.telerik.best_style_ltd.entities.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class UserRepositoryImpl implements  UserRepository {
+    private SessionFactory factory;
+
+    @Autowired
+    UserRepositoryImpl(SessionFactory factory) {
+        this.factory = factory;
+    }
+
     @Override
     public void addUser(User user) {
+        try (Session session = factory.openSession()) {
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 
@@ -17,12 +37,28 @@ public class UserRepositoryImpl implements  UserRepository {
 
     @Override
     public List<User> listUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try (Session session = factory.openSession()) {
+            session.beginTransaction();
+            users = session.createQuery("from User").list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return users;
     }
 
     @Override
     public User getUserById(int id) {
-        return null;
+        User user = null;
+        try (Session session = factory.openSession();) {
+            session.beginTransaction();
+            user = session.get(User.class, id);
+            session.getTransaction().commit();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return user;
     }
 
     @Override
